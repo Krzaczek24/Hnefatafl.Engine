1c74bfeac6d817204083a5139bcd0d4f1c27292a
Tomasz 'Krzaczek' Drewek <tomaszdrewek94@gmail.com>
2025-10-24 21:50:23 +0200

    Added connector tests project

---
+++ Hnefatafl.Engine.OnlineConnector.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Hnefatafl.Engine.OnlineConnector.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
